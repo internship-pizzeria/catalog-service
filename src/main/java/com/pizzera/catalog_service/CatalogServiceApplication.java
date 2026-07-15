47cbd60c42a359ad1abd6ddf11228af873886060
Hubert Jaskuła <hubertjaskula516@gmail.com>
2026-07-15 13:17:11 +0200

    refactor: remove unused Feign client dependencies and annotations to use our own client

--- src/main/java/com/pizzera/catalog_service/CatalogServiceApplication.java
+++ src/main/java/com/pizzera/catalog_service/CatalogServiceApplication.java
@@ -3,11 +3,9 @@ package com.pizzera.catalog_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableCaching
-@EnableFeignClients
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
