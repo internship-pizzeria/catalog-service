46ff667831088b2d0eec85eaba5cb280381b304b
Hubert Jaskuła <hubertjaskula516@gmail.com>
2026-07-20 12:18:53 +0200

    feat: add ingredient management entities including Ingredient, IngredientCategory, LocationIngredient, and ProductIngredient

---
+++ src/main/java/com/pizzera/catalog_service/product/LocationIngredient.java
@@ -0,0 +1,29 @@
+package com.pizzera.catalog_service.product;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "location_ingredients")
+public class LocationIngredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "location_id", nullable = false)
+    private Long locationId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ingredient_id", nullable = false)
+    private Ingredient ingredient;
+
+    @Column(nullable = false)
+    private boolean available = true;
+}
---
+++ src/main/java/com/pizzera/catalog_service/product/ProductIngredient.java
@@ -0,0 +1,29 @@
+package com.pizzera.catalog_service.product;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Table(name = "product_ingredients")
+public class ProductIngredient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ingredient_id", nullable = false)
+    private Ingredient ingredient;
+
+
+}
