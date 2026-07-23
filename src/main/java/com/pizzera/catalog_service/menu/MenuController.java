58907e7ad0de7716df4f4922c5032c82a2b1c99b
Hubert Jaskuła <hubertjaskula516@gmail.com>
2026-07-23 09:27:42 +0200

    refactor: remove unused imports in MenuController

--- src/main/java/com/pizzera/catalog_service/menu/MenuController.java
+++ src/main/java/com/pizzera/catalog_service/menu/MenuController.java
@@ -1,13 +1,10 @@
 package com.pizzera.catalog_service.menu;
 
-import com.pizzera.catalog_service.product.ProductResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/menu")
